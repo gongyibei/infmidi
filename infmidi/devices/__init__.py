ef0185463a266502e98975e4fce8e715752b3e22
gongyibei <gongyibei@gmail.com>
2022-12-08 17:16:10 +0800

    add "__all__"

--- infmidi/devices/__init__.py
+++ infmidi/devices/__init__.py
@@ -1,6 +1,9 @@
 from .controller import *
 from .fluidsynth import *
+from .viewer import *
 
 __all__ = []
 __all__.extend(controller.__all__)
 __all__.extend(fluidsynth.__all__)
+__all__.extend(viewer.__all__)
+
